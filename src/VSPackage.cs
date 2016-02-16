86c9da867e3f3974a73c103eb95e0e4de47f265d
Mads Kristensen <post@madskristensen.net>
2016-02-16 10:23:17 -0800

    Refactorings

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -5,22 +5,20 @@ using Microsoft.VisualStudio.Shell;
 namespace OpenInSublimeText
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", Version, IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideOptionPage(typeof(Settings), "Open in SublimeText", "General", 101, 111, true, new string[0], ProvidesLocalizedCategoryName = false)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.guidPackageString)]
     public sealed class VSPackage : Package
     {
-        public const string Version = "1.0";
-        public const string Title = "Open in Sublime Text";
         public static Settings Settings;
 
         protected override void Initialize()
         {
             Settings = (Settings)GetDialogPage(typeof(Settings));
 
-            Logger.Initialize(this, Title);
-            Telemetry.Initialize(this, Version, "7f5bc7fb-da06-481e-b66d-40088746d163");
+            Logger.Initialize(this, Vsix.Name);
+            Telemetry.Initialize(this, Vsix.Version, "7f5bc7fb-da06-481e-b66d-40088746d163");
             OpenSublimeTextCommand.Initialize(this);
 
             base.Initialize();
---
+++ src/source.extension.cs
@@ -0,0 +1,12 @@
+namespace OpenInSublimeText
+{
+	static class Vsix
+	{
+		public const string Id = "2925bbf7-d48b-4abd-83dc-1cd711d9b9ba";
+		public const string Name = "Open in Sublime Text";
+		public const string Description = "Adds a menu command that lets you open any solution, project, folder and file in Sublime Text.";
+		public const string Version = "1.0";
+		public const string Author = "Mads Kristensen";
+		public const string Tags = "sublime, text, code";
+	}
+}
